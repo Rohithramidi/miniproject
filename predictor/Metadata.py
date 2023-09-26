896f5de4f4735408eb271494339ec878ef554460
Rohithramidi <145133658+Rohithramidi@users.noreply.github.com>
2023-09-26 16:47:49 +0530

    Update Metadata.py

--- predictor/Metadata.py
+++ predictor/Metadata.py
@@ -1,57 +1,53 @@
+import librosa
+import numpy as np
+
 def getmetadata(filename):
-    import librosa
-    import numpy as np
-
-
+    # Load the audio file
     y, sr = librosa.load(filename)
-    #fetching tempo
 
-    onset_env = librosa.onset.onset_strength(y, sr)
-    tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
+    # Calculate onset strength
+    onset_env = librosa.onset.onset_strength(y=y)
 
-    #fetching beats
+    # Calculate tempo and beats
+    tempo, beat_frames = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
 
+    # Calculate harmonic and percussive components
     y_harmonic, y_percussive = librosa.effects.hpss(y)
-    tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,sr=sr)
 
-    #chroma_stft
-
+    # Calculate chroma STFT
     chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
 
-    #rmse
-
+    # Calculate RMSE
     rmse = librosa.feature.rms(y=y)
 
-    #fetching spectral centroid
+    # Calculate spectral centroid
+    spec_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
 
-    spec_centroid = librosa.feature.spectral_centroid(y, sr=sr)[0]
-
-    #spectral bandwidth
-
+    # Calculate spectral bandwidth
     spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
 
-    #fetching spectral rolloff
+    # Calculate spectral rolloff
+    spec_rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
 
-    spec_rolloff = librosa.feature.spectral_rolloff(y+0.01, sr=sr)[0]
-
-    #zero crossing rate
-
+    # Calculate zero crossing rate
     zero_crossing = librosa.feature.zero_crossing_rate(y)
 
-    #mfcc
-
+    # Calculate MFCCs
     mfcc = librosa.feature.mfcc(y=y, sr=sr)
 
-    #metadata dictionary
+    # Metadata dictionary
+    metadata_dict = {
+        'tempo': tempo,
+        'chroma_stft': np.mean(chroma_stft),
+        'rmse': np.mean(rmse),
+        'spectral_centroid': np.mean(spec_centroid),
+        'spectral_bandwidth': np.mean(spec_bw),
+        'rolloff': np.mean(spec_rolloff),
+        'zero_crossing_rates': np.mean(zero_crossing)
+    }
 
-    metadata_dict = {'tempo':tempo,'chroma_stft':np.mean(chroma_stft),'rmse':np.mean(rmse),
-                     'spectral_centroid':np.mean(spec_centroid),'spectral_bandwidth':np.mean(spec_bw), 
-                     'rolloff':np.mean(spec_rolloff), 'zero_crossing_rates':np.mean(zero_crossing)}
-
-    for i in range(1,21):
-        metadata_dict.update({'mfcc'+str(i):np.mean(mfcc[i-1])})
+    # Add MFCCs to the dictionary
+    for i in range(1, 21):
+        metadata_dict[f'mfcc{i}'] = np.mean(mfcc[i - 1])
 
     return list(metadata_dict.values())
-
-
-
